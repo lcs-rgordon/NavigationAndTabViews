250adcb46cd3a52a40a8b984f9ca8063c2555440
Russell Gordon <rgordon@lcs.on.ca>
2023-02-08 15:11:16 -0500

    Added a view for the second tab (not actually using this view yet though).

---
+++ NavigationAndTabViews/Views/TabTwoView.swift
@@ -0,0 +1,22 @@
+//
+//  TabTwoView.swift
+//  NavigationAndTabViews
+//
+//  Created by Russell Gordon on 2023-02-08.
+//
+
+import SwiftUI
+
+struct TabTwoView: View {
+    var body: some View {
+        Text("Tab 2")
+            .font(.largeTitle)
+            .fontWeight(.black)
+    }
+}
+
+struct TabTwoView_Previews: PreviewProvider {
+    static var previews: some View {
+        TabTwoView()
+    }
+}
