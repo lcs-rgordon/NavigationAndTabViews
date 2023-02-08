e949d2c7475f6fcb1243551ca98f7ef4a4097264
Russell Gordon <rgordon@lcs.on.ca>
2023-02-08 15:07:54 -0500

    Initial Commit

---
+++ NavigationAndTabViews/NavigationAndTabViewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationAndTabViewsApp.swift
+//  NavigationAndTabViews
+//
+//  Created by Russell Gordon on 2023-02-08.
+//
+
+import SwiftUI
+
+@main
+struct NavigationAndTabViewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
