5c24b0d24ccca8eaa7b970bcc4301f991fd09941
Russell Gordon <rgordon@lcs.on.ca>
2023-02-08 15:10:16 -0500

    Removed ContentView, added TabOneView (although not actually using tabs yet).

---
+++ NavigationAndTabViews/Views/TabOneView.swift
@@ -0,0 +1,22 @@
+//
+//  TabOneView.swift
+//  NavigationAndTabViews
+//
+//  Created by Russell Gordon on 2023-02-08.
+//
+
+import SwiftUI
+
+struct TabOneView: View {
+    var body: some View {
+        Text("Tab 1")
+            .font(.largeTitle)
+            .fontWeight(.black)
+    }
+}
+
+struct TabOneView_Previews: PreviewProvider {
+    static var previews: some View {
+        TabOneView()
+    }
+}
